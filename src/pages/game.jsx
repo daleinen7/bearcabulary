b48d13effeefcf0a74a29fd6f4c278099d3837fa
daleinen7 <daleinen@gmail.com>
2021-07-23 17:28:20 -0500

    add prettier config file

---
+++ src/pages/game.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function Game() {
+  return (
+    <Layout>
+      <h2>The game component</h2>
+    </Layout>
+  )
+}
