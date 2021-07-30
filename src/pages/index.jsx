27534254937714555afdaa1b80880753eff46165
daleinen7 <daleinen@gmail.com>
2021-07-29 21:33:00 -0500

    scss modules started

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
+import '../styles/style.scss';
 
 export default function Index() {
   return (
