71fdfb60e9e8f91540361da9d44489ebce3d677e
daleinen7 <daleinen@gmail.com>
2021-07-31 15:56:53 -0500

    pull data from NetlifyCMS via GraphQL

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Layout from '../components/Layout';
-import '../styles/style.scss';
+import React from "react";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import Layout from "../components/Layout";
+import "../styles/style.scss";
 
 export default function Index() {
+  const data = useStaticQuery(graphql`
+    query Stories {
+      allStoriesJson {
+        nodes {
+          title
+          parent {
+            ... on File {
+              name
+            }
+          }
+          section {
+            img
+            sentence
+            word
+          }
+        }
+      }
+      storiesJson {
+        parent {
+          ... on File {
+            id
+            name
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
       <h2>This is the landing page</h2>
-      <Link to="/game/">Start Game</Link>
+      {data.allStoriesJson.nodes.map((node) => {
+        return (
+          <ul>
+            <li>
+              <Link to={node.parent.name}>{node.title}</Link>
+            </li>
+          </ul>
+        );
+      })}
     </Layout>
   );
 }
