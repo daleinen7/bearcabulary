cca4b91ca9ea95241c0606e4d60f7a47e9649a4e
Kaung Shein <nicholasling1412@gmail.com>
2021-08-02 22:17:05 -0400

    Removed footer + changed sizing to accomodate

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import Header from '../Header';
-import Footer from '../Footer';
+import React from "react";
+import Header from "../Header";
+// import Footer from '../Footer';
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
       {children}
-      <Footer />
     </>
   );
 }
