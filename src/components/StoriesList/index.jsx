94a03882ebf77394d9fd101d1a3c7acd8b4d78fb
daleinen7 <daleinen@gmail.com>
2021-08-02 23:14:49 -0500

    story cards started

--- src/components/StoriesList/index.jsx
+++ src/components/StoriesList/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "gatsby";
+import * as styles from "./StoriesList.module.scss";
 
 export default function StoriesList({ stories }) {
+  console.log(stories);
   return (
-    <ul>
+    <ul className={styles.stories_list}>
       {stories.map((story, index) => {
         return (
           <li key={index}>
+            <img src={story.section[0].img} alt={story.title} />
             <Link to={story.parent.name}>{story.title}</Link>
           </li>
         );
