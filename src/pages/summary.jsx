ea6bf55dd99aaf4ff9ba8ed221c7da19f9acbc6d
Kaung Shein <nicholasling1412@gmail.com>
2021-08-05 17:20:38 -0400

    Fixed bug

--- src/pages/summary.jsx
+++ src/pages/summary.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "gatsby";
 
 export default function Summary({ location }) {
-  console.log(location);
-  return (
-    <>
-      {location.state.corrects.map((word, index) => {
-        return (
-          <div key={index}>
-            {word}{" "}
-            {location.state.errors[word]?.map((error, index2) => {
-              return <div key={index2}>{error}</div>;
-            })}
-          </div>
-        );
-      })}
-      <div>Summary</div>
-      <Link to="/">Go to Home</Link>
-    </>
-  );
+  if (!location.state) {
+    return <div>You seem lost.</div>;
+  } else {
+    return (
+      <>
+        {location.state.corrects.map((word, index) => {
+          return (
+            <div key={index}>
+              {word}{" "}
+              {location.state.errors[word]?.map((error, index2) => {
+                return <div key={index2}>{error}</div>;
+              })}
+            </div>
+          );
+        })}
+        <div>Summary</div>
+        <Link to="/">Go to Home</Link>
+      </>
+    );
+  }
 }
