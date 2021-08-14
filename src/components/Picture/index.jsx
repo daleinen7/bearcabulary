e01bb5fae909497d226cbb265ca3b334bc1c9c0b
daleinen7 <daleinen@gmail.com>
2021-08-14 14:47:01 -0500

    images in stories through gatsby image

--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import * as styles from "./Picture.module.scss";
+import { GatsbyImage } from "gatsby-plugin-image";
 
-export default function Picture({ picture, sentence }) {
+export default function Picture({ picture, sentence, image }) {
+  console.log("Image FILESLHREAHR EAHR", image);
   return (
     <div className={styles.image_container}>
-      <img className={styles.image} src={picture} alt={sentence} />
+      <GatsbyImage image={image} className={styles.image} alt={sentence} />
+      {/* <img className={styles.image} src={picture} alt={sentence} /> */}
     </div>
   );
 }
---
+++ src/utilities/imageSelectionUtil.js
@@ -0,0 +1,12 @@
+export const findImage = (images, story, counter) => {
+  console.log("images", images);
+  console.log("story", story);
+  console.log("counter", counter);
+
+  const result = images.find(
+    (ele) => ele.relativePath === story.section[counter].media.slice(5)
+  ).childrenImageSharp[0].gatsbyImageData;
+
+  console.log(result);
+  return result;
+};
