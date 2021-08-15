c4d4b34cc942246faf5fd027c079d3c378c780eb
Kaung Shein <nicholasling1412@gmail.com>
2021-08-15 18:33:22 -0400

    disabled placeholder color

--- src/pages/{storiesJson.title}.jsx
+++ src/pages/{storiesJson.title}.jsx
@@ -286,7 +286,7 @@ export const query = graphql`
       nodes {
         relativePath
         childrenImageSharp {
-          gatsbyImageData(width: 1000)
+          gatsbyImageData(width: 1000, placeholder: NONE)
         }
       }
     }
