7bf2a1ffa9d5112894db030a0c41df6b5dbdb28b
Kaung Shein <nicholasling1412@gmail.com>
2021-08-16 02:48:27 -0400

    refactored speech without a library

--- src/components/TextToSpeech/index.jsx
+++ src/components/TextToSpeech/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import Speech from "react-speech";
+import * as styles from "./TextToSpeech.module.scss";
+import { FaPlay } from "react-icons/fa";
 
 export default function TextToSpeech({ word }) {
+  const utterThis = new SpeechSynthesisUtterance(word);
+  utterThis.pitch = 1.5;
+  utterThis.rate = 0.7;
+  utterThis.voice = window.speechSynthesis.getVoices()[0];
+
   return (
     <>
-      <div>Speech</div>
-      <Speech text={word} pitch="1.5" rate="0.7" />
+      <button
+        className={styles.speech_button}
+        onClick={() => {
+          window.speechSynthesis.cancel();
+          window.speechSynthesis.speak(utterThis);
+        }}
+      >
+        <FaPlay />
+      </button>
     </>
   );
 }
