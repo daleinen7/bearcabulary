91fbfdd7c10875ecc83ca7f3b50520388898466d
Kaung Shein <nicholasling1412@gmail.com>
2021-08-16 00:14:43 -0400

    Capitalize first letter + initialize styling

--- src/components/Definition/index.jsx
+++ src/components/Definition/index.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import * as styles from "./Definition.module.scss";
 
 export default function Definition({ word }) {
   const [definition, setDefinition] = useState("");
@@ -9,11 +10,15 @@ export default function Definition({ word }) {
         const result = await fetch(
           `https://www.dictionaryapi.com/api/v3/references/sd2/json/${word}?key=${process.env.GATSBY_MERRIAM_APIKEY}`
         ).then((res) => res.json());
-        setDefinition(result[0].shortdef[0]);
+        setDefinition(
+          result[0].shortdef[0].replace(/^\w/, (firstLetter) =>
+            firstLetter.toUpperCase()
+          )
+        );
       }
       fetchData();
     }
   }, [word]);
 
-  return <div>{definition}</div>;
+  return <div className={styles.definition_container}>{definition}.</div>;
 }
