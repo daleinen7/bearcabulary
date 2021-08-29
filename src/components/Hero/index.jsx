5a575760d2e7af89002e88cdbacbdf7de191dcfb
daleinen7 <daleinen@gmail.com>
2021-08-29 17:59:23 -0500

    letters in block

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,29 +1,13 @@
 import React from "react";
 import * as styles from "./hero.module.scss";
-import banner from "./banner.svg";
 
 export default function Hero() {
+  const title = "Bearcabulary".split("").map((char) => {
+    return <span className={styles.letters}>{char}</span>;
+  });
   return (
-    <section className={styles.hero}>
-      <div className={styles.img_and_copy}>
-        <img
-          src={banner}
-          className={styles.banner}
-          alt="Bearcabulary is a journey"
-        />
-        <div className={styles.copy}>
-          <p>
-            Bearcabulary offers games for kindergarten through 3rd grade
-            students. Each story incorporates interactive spelling challenges
-            and fun quizzes for your child to have an educational adventure!
-          </p>
-          <p>
-            All stories were custom built with the supplied CMS. Find us on{" "}
-            <a href="https://github.com/daleinen7/bearcabulary">Github</a> for
-            more details. Select a story below to see Bearcabulary in action!
-          </p>
-        </div>
-      </div>
+    <section>
+      <h1>{title}</h1>
     </section>
   );
 }
