f65dd80e33a7010d533be077b100e8f5231e4afa
daleinen7 <daleinen@gmail.com>
2021-09-21 15:19:04 -0500

    meta description

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,15 +5,31 @@ import { Helmet } from "react-helmet";
 export default function Layout({ children, title, location }) {
   return (
     <>
-      <Helmet>
+      <Helmet
+        htmlAttribute={{lang: 'en'}}
+        title={{title:`Bearcabulary ${title ? " • " + title : ""}`}}
+        meta={[
+          {name: 'description'},
+          {content: "Bearcabulary is a word game for children K-3. Bearcabulary tests students to spell a missing word in a sentence using jumbled letters."}
+        ]}
+      />
         <meta charSet="utf-8" />
         <title>{`Bearcabulary ${title ? " • " + title : ""}`}</title>
-        <meta name="description" content="Word game for children K-3." />
+        <meta name="description" content="Word game for children K-3. Bearcabulary tests students to spell a missing word in a sentence. Students will see the word briefly before " />
         <link
           rel="canonical"
           href={`https://bearcabulary.com/${location.pathname}`}
         />
       </Helmet>
+      {/* <Helmet>
+        <meta charSet="utf-8" />
+        <title>{`Bearcabulary ${title ? " • " + title : ""}`}</title>
+        <meta name="description" content="Word game for children K-3. Bearcabulary tests students to spell a missing word in a sentence. Students will see the word briefly before " />
+        <link
+          rel="canonical"
+          href={`https://bearcabulary.com/${location.pathname}`}
+        />
+      </Helmet> */}
       <Header />
       {children}
     </>
